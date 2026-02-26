9bbf5055e3377ca223da5377d9cce5e613927d8a
AshrafGit256 <ashrafwalusimbi27@gmail.com>
2026-02-26 12:14:54 +0300

    Debugged the register.cshtml page

--- obj/Debug/net10.0/EventManagementPortal.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventManagementPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagementPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7d874de0b259246bfced1caa4246a726c98e633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62137f10b6d9031d785c2344515bd3d4eef8be1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagementPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagementPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
