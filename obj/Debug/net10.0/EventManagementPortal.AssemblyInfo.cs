aeec77850e0a1412e4f86ab9d1fd9f1e48f56946
AshrafGit256 <ashrafwalusimbi27@gmail.com>
2026-02-26 22:53:37 +0300

    Created ManageOrganizers.cshtml razor view

--- obj/Debug/net10.0/EventManagementPortal.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventManagementPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagementPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857d559a4423f869b3092d904414a6597bbce4db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de1a63d69cf692c70f47dac14255ac737d41c50")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagementPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagementPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
